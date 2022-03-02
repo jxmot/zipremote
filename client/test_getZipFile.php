32d3c681456a6f6a7a3c8f4087d8022686c20a18
jxmot <webdev@stupiddog.net>
2022-03-02 13:34:56 -0600

    added comments

--- client/test_getZipFile.php
+++ client/test_getZipFile.php
@@ -1,6 +1,13 @@
 <?php
 require_once './zipremote.php';
 
+/*
+
+    'bigsite' - this is the site ID, it identifies which server to access
+    'key_goes_here' - this is your key, it must match both apikeys.json files
+    'recur' - specifies which "zip target" to zip up, and the name of the zip file
+
+*/
 // get the files...
 $ret = getZipFile('bigsite', 'key_goes_here', 'recur');
 // did we get a numeric return code?
--- client/test_putZipFile.php
+++ client/test_putZipFile.php
@@ -1,6 +1,16 @@
 <?php
 require_once './zipremote.php';
 
+/*
+
+    'bigsite' - this is the site ID, it identifies which server to access
+    'key_goes_here' - this is your key, it must match both apikeys.json files
+    'uztest3' - this is the unzip target ID, it will provide the path and 
+                default file pattern(s) used when extracting files
+    'recursmall_sub.zip' - the name of the zip file being uploaded
+    '*.log' - an client optional file pattern used when extracting files
+
+*/
 // get the files...
 $ret = putZipFile('bigsite', 'key_goes_here', 'uztest3', 'recursmall_sub.zip', '*.log');
 // did we get a numeric return code?
